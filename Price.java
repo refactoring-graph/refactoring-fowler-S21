87780f7fff49e38ecb14ab9819fbd5f9ff3aaa83
Rafael Oliveira <rafael-goncalves-oliveira@outlook.com>
2020-10-19 16:03:29 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -3,13 +3,7 @@ public abstract class Price {
 
   public abstract double getCharge(int daysRented);
 
-  public int getFrequentRenterPoints(int daysRented) {
-    // add frequent renter points
-    // add bonus for a two day new release rental
-    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-      return 2;
-    }
-    
+  public int getFrequentRenterPoints(int daysRented) {    
     return 1;
   }
 }
